02c8c284e58e84bfed6d722e87612d6d041a2482
Sebastián Córdova <sebastiancordovaw@gmail.com>
2023-04-02 19:51:14 -0400

    primer commit

---
+++ database/migrations/2023_04_02_230651_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->text('description',255);
+            $table->double('price', 8, 2);
+            $table->string('measure',255);
+            $table->double('quantityUnitMeasure', 8, 2);
+            $table->double('stock', 8, 2);
+            $table->boolean('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
