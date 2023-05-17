c2d178dd44e847eb18ea0d99ebff93bae6b5d87f
sebastiancordovaw <sebastiancordovaw@gmail.com>
2023-05-17 06:45:02 +0200

    dokerizado

--- resources/js/Pages/Tables.vue
+++ resources/js/Pages/Tables.vue
@@ -263,7 +263,6 @@ export default{
             </h2>
         </template>
         -->
-
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-xl">
